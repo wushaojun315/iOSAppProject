b3f5045386e67b72bfe508ca5ecdb324205bcb51
吴少军 <1113750080@qq.com>
2016-11-02 10:10:20 +0800

    Initial Commit

---
+++ APPProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APPProject
+//
+//  Created by 吴少军 on 2016/11/2.
+//  Copyright © 2016年 George. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
